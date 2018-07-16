721add9c223c06662710a873b4cd916f16f8b10c
Antony Holmes <antony.b.holmes@gmail.com>
2018-07-16 15:42:23 -0400

    Initial commit.

---
+++ edu.columbia.rdf.matcalc.toolbox.mcr.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/mcr/app/MCRIcon.java
@@ -0,0 +1,10 @@
+package edu.columbia.rdf.matcalc.toolbox.mcr.app;
+
+import org.jebtk.modern.ColorTheme;
+import org.jebtk.modern.graphics.icons.ModernApplicationIcon;
+
+public class MCRIcon extends ModernApplicationIcon {
+  public MCRIcon() {
+    super("P", "r", ColorTheme.ORANGE);
+  }
+}
